659c9f98e8aa99ad9de17b6c98e27e98accb3e92
Maxim <maximchumbaev@gmail.com>
2018-03-24 02:49:54 +0300

    chart grid

--- graph/graph.py
+++ graph/graph.py
@@ -15,10 +15,15 @@ def render_course_enrollments(data: list):
                 'x': 0
             })
 
-    for course in [d['название курса'] for d in data]:
+    all_keys = [d['название курса'] for d in data]
+
+    for course in all_keys:
         for item in items:
             if item['y'] == course:
                 item['x'] += 1
+
+    items = zip(*[iter(items)]*10)
+    items = [list(item) for item in zip(items)]
 
     def x(row, index):
         return row['x']
@@ -26,13 +31,23 @@ def render_course_enrollments(data: list):
     def y(row, index):
         return row['y']
 
-    chart = leather.Chart('Записи на курсы')
-    chart.add_bars(items, x=x, y=y)
+    chart_grid = leather.Grid()
+
+    charts = []
+    for item in items:
+        charts.append(leather.Chart('Записи на курсы'))
+        charts[-1].add_bars(item, x=x, y=y)
+        chart_grid.add_one(charts[-1])
+
+    # chart = leather.Chart('Записи на курсы')
+    # chart.add_bars(items, x=x, y=y)
 
     filename = f"enrollments_graph_" \
                f"{datetime.now(tz=pytz.timezone('Europe/Moscow')).strftime('%d%m%Y_%H%M')}.svg"
 
-    chart.to_svg(f'/data/{filename}')
+    chart_grid.to_svg(f'/data/{filename}')
+
+    # chart.to_svg(f'/data/{filename}')
     return filename
 
 
